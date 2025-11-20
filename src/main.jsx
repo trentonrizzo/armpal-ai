f71fd8304e522fe3e6437f61f896fe5305bb58a3
trentonrizzo <trentonrizzo15@gmail.com>
2025-11-20 14:50:45 -0600

    fix: remove merge conflict from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./App";
-import "./index.css";
-=======
 import { BrowserRouter } from "react-router-dom";
->>>>>>> d2232ea (Added progress graph page)
-
 import App from "./App";
-import { AppProvider } from "./context/AppContext";
-
 import "./index.css";
+
+// PWA Registration
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App />
-=======
-    <AppProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AppProvider>
->>>>>>> d2232ea (Added progress graph page)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
